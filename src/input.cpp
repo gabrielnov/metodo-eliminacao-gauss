b57b3eaf76b49665ad8bd3ffbcf1e81feabee05f
Gabriel <gc.novaes@outlook.com>
2023-03-10 11:08:19 -0300

    remove arquivos e adiciona metodos de input

--- src/input.cpp
+++
@@ -1,9 +0,0 @@
-#include "input.hpp"
-
-int sizeInput(){
-	int ordem;
-
-	std::cout << "Informe a ordem da matriz: ";
-	std::cin >> ordem;
-	return ordem;
-}
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include "input.hpp"
 
 int main(){
-	int ordem = sizeInput();
+	int size, **matriz;
+	size = sizeInput();
 
 	return 0;
+}
+
+int sizeInput(){
+	int ordem;
+
+	std::cout << "Informe a ordem da matriz: ";
+	std::cin >> ordem;
+	return ordem;
+}
+
+void matrixInput(int **matriz, int size){
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++){
+			std::cout << "informe o valor da posição " << i << " x " << j << std::endl;
+			std::cin >> matriz[i][j];
+		}
+}
+
+void solve(int **matriz){
+	
 }

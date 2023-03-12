9ab07d5446a26311e9e6d1cc01c4cda90e7a56d9
Gabriel <gc.novaes@outlook.com>
2023-03-12 20:26:25 -0300

    adiciona funcao para zerar termos

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,40 @@
 #include "main.hpp"
 #include <iostream>
+#include <iomanip> 
+#include <cmath>
 
 int main(){
 	int size;
-	float **m, *vetor;
+	double **m, *vetor;
 	
-	size = sizeInput();
+	// size = sizeInput();
+	size = 3;
 
-	m = new float * [size];
+	m = new double * [size];
 	for (int i = 0; i < size; i++)
-		m[i] = new float[size+1];
+		m[i] = new double[size+1];
 	
-	vetor = new float [size];
+	vetor = new double [size];
 
-	matrixInput(m, size);
-	vectorInput(vetor, size);
-	concatenateMatrix(m, vetor, size);
+	// matrixInput(m, size);
+	// vectorInput(vetor, size);
+	vetor[0] = 1;
+	vetor[1] = 2;
+	vetor[2] = 3;
+
+	m[0][0] = 3;
+	m[0][1] = 2;
+	m[0][2] = 4;
 	
+	m[1][0] = 1;
+	m[1][1] = 1;
+	m[1][2] = 2;
+	
+	m[2][0] = 4;
+	m[2][1] = 3;
+	m[2][2] = -2;
+
+	concatenateMatrix(m, vetor, size);
 	printMatrix(m, size);
 	solve(m, size);
 	printMatrix(m, size);
@@ -38,7 +56,7 @@ int sizeInput(){
 	return ordem;
 }
 
-void matrixInput(float **m, int size){
+void matrixInput(double **m, int size){
 	std::cout << "Entrada de valores - matriz" << std::endl;
 	for (int i = 0; i < size; i++)
 		for (int j = 0; j < size; j++){
@@ -47,7 +65,7 @@ void matrixInput(float **m, int size){
 		}
 }
 
-void vectorInput(float *vetor, int size){
+void vectorInput(double *vetor, int size){
 	std::cout << "Entrada de valores - vetor" << std::endl;
 	for (int i = 0; i < size; i++){
 			std::cout << "informe o valor da posição " << i << std::endl;
@@ -55,18 +73,18 @@ void vectorInput(float *vetor, int size){
 	}
 }
 
-void concatenateMatrix(float **m, float *vetor, int size){
+void concatenateMatrix(double **m, double *vetor, int size){
 	for (int i = 0; i < size; i++){
 		m[i][size] = vetor[i];
 	}
 		
 }
 
-void printMatrix(float **m, int size){
+void printMatrix(double **m, int size){
 	std::cout << "\n";
 	for (int i = 0; i < size; i++){
 		for (int j = 0; j <= size; j++){
-			std::cout << m[i][j] << " ";
+			std::cout << std::setfill(' ') << std::setw(8) << std::fixed <<  std::setprecision(2) << m[i][j] ;
 		}
 		std::cout << "\n";
 	}
@@ -74,14 +92,19 @@ void printMatrix(float **m, int size){
 		
 }
 
-void solve(float **m, int size){
-	float **x; 
-	*x = new float [size];
+void solve(double **m, int size){
+	double f;
 
-	x = new float * [size];
-	for (int i = 0; i < size; i++)
-		x[i] = new float[size+1];
+	for (int i = 1; i < size; i++){
+		for (int j = 0; j < i; j++){
+			f = m[i][j] / m[j][j];
+			updateRow(m, i, f, size, j);
+		}
+	}
+}
 
-
-	delete[] x;
+void updateRow(double **m, int row, double factor, int size, int iteration){
+	for(int i = 0; i <= size; i++){
+		m[row][i] = m[row][i] - factor * m[iteration][i];
+	}
 }
--- src/main.hpp
+++ src/main.hpp
@@ -2,10 +2,18 @@
 #define __MAIN_H__
 
 int sizeInput();
-void matrixInput(float **m, int size);
-void vectorInput(float *vetor, int size);
-void printMatrix(float **m, int size);
-void concatenateMatrix(float **m, float *vetor, int size);
-void solve(float **m, int size);
+void matrixInput(double **m, int size);
+void vectorInput(double *vetor, int size);
+void printMatrix(double **m, int size);
+void concatenateMatrix(double **m, double *vetor, int size);
+void solve(double **m, int size);
+void updateRow(double **m, int row, double factor, int size, int iteration);
 int main();
+
+struct fraction {
+    int num;
+    int denom;
+    bool positive;
+};
+
 #endif
